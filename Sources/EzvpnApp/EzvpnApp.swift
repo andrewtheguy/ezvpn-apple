8f62508ba906340b74e2c5c338718d9fcdecdc8b
Andrew Chen <andrew@jaanuu.com>
2026-07-15 20:53:51 -0700

    feat: add macOS menu bar controls

--- Sources/EzvpnApp/EzvpnApp.swift
+++ Sources/EzvpnApp/EzvpnApp.swift
@@ -1,16 +1,31 @@
 import SwiftUI
 
+enum EzvpnScene {
+    static let mainWindowID = "main"
+}
+
 @main
 struct EzvpnApp: App {
     @StateObject private var manager = TunnelsManager()
 
     var body: some Scene {
+        #if os(macOS)
+        Window("ezvpn", id: EzvpnScene.mainWindowID) {
+            TunnelListView()
+                .environmentObject(manager)
+        }
+        .defaultSize(width: 480, height: 600)
+
+        MenuBarExtra("ezvpn", systemImage: "network") {
+            MenuBarView()
+                .environmentObject(manager)
+        }
+        .menuBarExtraStyle(.menu)
+        #else
         WindowGroup {
             TunnelListView()
                 .environmentObject(manager)
         }
-        #if os(macOS)
-        .defaultSize(width: 480, height: 600)
         #endif
     }
 }
